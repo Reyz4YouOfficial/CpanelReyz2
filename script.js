8435130389494636378315c1fe7564ecff024c2b
Reyz4You Official <raihanananda767@gmail.com>
2025-10-25 18:55:26 +0700

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // Typing animation
-const texts = ["Auto Panel Creator", "Pterodactyl API", "Gyzencode Developer", "Server Management"];
+const texts = ["Auto Panel Creator", "Pterodactyl API", "Reyz4You Developer", "Server Management"];
 let textIndex = 0;
 let charIndex = 0;
 const typingElement = document.getElementById("typing");
